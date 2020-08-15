23095e10a67282fc8c2fead410499eafa5dc9f2c
Dylan Bortnick <dbortnick56@gmail.com>
2020-08-15 12:55:45 -0400

    Initial Commit

---
+++ HomeworkHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeworkHelper
+//
+//  Created by Dylan Bortnick on 8/15/20.
+//  Copyright © 2020 BortnickDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
